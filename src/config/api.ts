5a099e38d0c67f68153e021755da69a3696c9035
Camiloruas <miloruas@gmail.com>
2026-04-21 21:01:55 -0300

    Update Details

--- src/config/api.ts
+++ src/config/api.ts
@@ -5,8 +5,16 @@ if (!COINCAP_API_KEY) {
   throw new Error('Configure VITE_COINCAP_API_KEY no arquivo .env');
 }
 
-export const COINCAP_ASSETS_URL = `${COINCAP_BASE_URL}/assets?${new URLSearchParams({
-  limit: '10',
-  offset: '0',
-  apiKey: COINCAP_API_KEY,
-})}`;
+export function getCoincapAssetsUrl(offset = 0) {
+  return `${COINCAP_BASE_URL}/assets?${new URLSearchParams({
+    limit: '10',
+    offset: String(offset),
+    apiKey: COINCAP_API_KEY,
+  })}`;
+}
+
+export function getCoincapAssetUrl(assetId: string) {
+  return `${COINCAP_BASE_URL}/assets/${assetId}?${new URLSearchParams({
+    apiKey: COINCAP_API_KEY,
+  })}`;
+}
--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -1,7 +1,19 @@
+import { useParams } from 'react-router-dom';
+import styles from './detail.module.css';
+import { useEffect } from 'react';
+
 export function Detail() {
+  const { cripto } = useParams;
+
+  useEffect(() => {
+    async function getCoin() {
+      try {
+      } catch (error) {}
+    }
+  }, [cripto]);
   return (
     <div>
-      <h1>Página Detalhe da moeda</h1>
+      <h1>Página Detalhada de Moeda </h1>
     </div>
   );
 }
